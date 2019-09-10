3e1de476c5bd40617dc2403a33ca60515ff78139
MaksimZotov <zotovmaksim1254@gmail.com>
2019-09-10 20:11:08 +0300

    lesson5

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -189,7 +189,28 @@ fun whoAreInBoth(a: List<String>, b: List<String>): List<String>
  *     mapOf("Emergency" to "911", "Police" to "02")
  *   ) -> mapOf("Emergency" to "112, 911", "Police" to "02")
  */
-fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> = TODO()
+fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> {
+    var map: MutableMap<String, String> = mutableMapOf();
+    for ((keyA, valueA) in mapA) {
+        map[keyA] = valueA                             // Копирование mapA в map
+        for ((keyB, valueB) in mapB)
+            if (keyA == keyB && valueA != valueB) {    // Проверка на совпадение элементов а и b
+                map[keyA] = "$valueA, $valueB";
+            }
+    }
+    var count = 0;
+    for ((keyB, valueB) in mapB) {                     // Добавление недостающих элементов b
+        for ((key, value) in map) {
+            count++;
+            if (key == keyB)
+                break
+            if (count == map.size && key != keyB)
+                map[keyB] = valueB;
+        }
+        count = 0;
+    }
+    return map;
+}
 
 /**
  * Средняя
@@ -201,7 +222,22 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> {
+    var map: MutableMap<String, Double> = mutableMapOf();
+    for (item in stockPrices) {
+        var exist = false;
+        for ((key, value) in map) {
+            if (key == item.first) {
+                exist = true;
+                break
+            }
+        }
+        if (exist) {
+            //var = item.second + map[item.first];
+        }
+    }
+    return map;
+}
 
 /**
  * Средняя
