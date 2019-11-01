752e01512dd3f7f4b3584760670e2d4d6c0f7759
MaksimZotov <zotovmaksim1254@gmail.com>
2019-11-01 12:03:28 +0300

    lesson 9

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -2,6 +2,8 @@
 
 package lesson9.task1
 
+import lesson7.task1.chooseLongestChaoticWord
+
 /**
  * Ячейка матрицы: row = ряд, column = колонка
  */
@@ -41,32 +43,50 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = MatrixImpl(height, width, e)
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
+    private val list = List(height) { MutableList(width) { e } }
 
-    override val width: Int = TODO()
+    override fun get(row: Int, column: Int): E = list[row][column]
 
-    override fun get(row: Int, column: Int): E = TODO()
-
-    override fun get(cell: Cell): E = TODO()
+    override fun get(cell: Cell): E = get(cell.row, cell.column)
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        list[row][column] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        set(cell.row, cell.column, value)
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (!(other is MatrixImpl<*> && height == other.height && width == other.width)) return false
+        for (i in list.indices)
+            for (j in list[i].indices)
+                if (list[i][j] != other[i, j]) return false
+        return true
+    }
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        for (row in 0..height - 1) {
+            sb.append("[")
+            for (column in 0..width - 1) {
+                sb.append(this[row, column])
+                if (column != width - 1) sb.append("], ")
+                else sb.append("]")
+            }
+            sb.append("]")
+        }
+        sb.append("]")
+        return "$sb" // or, sb.toString()
+    }
 }
 
